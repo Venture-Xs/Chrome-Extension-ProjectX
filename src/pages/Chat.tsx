03c2b2483c506b671fdce1f6fc32287c2b51d5e1
Anish Pillai <anishp8914@gmail.com>
2024-02-25 04:01:56 +0530

    voice added

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,3 +1,4 @@
+import { VoiceReply } from "../utils/VoiceReply"
 import { SearchBar } from "../utils/SearchBar"
 //import Search from "../utils/Search"
 
@@ -9,8 +10,9 @@ export const Chat = () => {
 
         <div className="h-[400px] mt-5 p-2 bg-red flex relative items-center justify-center">
 
-            <div className=" bg-slate-500">
-                    chat
+            
+            <div >
+                <VoiceReply/>
             </div>
 
             <div className="h-[62px] p-2 rounded bg-slate-50 flex  absolute bottom-1">
---
+++ src/utils/VoiceReply.tsx
@@ -0,0 +1,22 @@
+
+export const VoiceReply = () => {
+    
+    
+      
+
+
+    return(
+        
+
+<div>
+      
+      <audio controls autoPlay >
+        <source src="assets/voice.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  
+    );
+
+
+}
