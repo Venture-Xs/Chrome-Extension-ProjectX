439ede13fd3351989a5cfd60e706435e3e72eac2
Dennis Johns <99817065+djohns1011@users.noreply.github.com>
2024-02-24 18:16:07 +0530

    created search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 //import { useState } from 'react'
-
-import { DropDown, } from "./utils/DropDown"
-import { SettingButton } from "./utils/SettingButton"
-
-
+import Search from "./utils/Search";
+import { DropDown } from "./utils/DropDown";
+import { SettingButton } from "./utils/SettingButton";
 
 function App() {
   //const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className="h-90px bg-slate-300 overflow-visible">
+        <div className="flex justify-evenly h-22 p-5 bg-slate-400 ">
+          <h1 className="text-4xl font-serif italic  text-black-400 p-2">
+            CAITYLN
+          </h1>
 
-     <div className="h-90px bg-slate-300 overflow-visible">
-      <div className="flex justify-evenly h-22 p-5 bg-slate-400 ">
-        <h1 className="text-4xl font-serif italic  text-black-400 p-2">CAITYLN</h1>
+          <div className=" flex justify-center h-18 w-32 p-2">
+            <DropDown />
+          </div>
 
-        <div className=" flex justify-center h-18 w-32 p-2">
-            <DropDown/>
+          <div className="flex justify-center h-18 w-18  p-2">
+            <SettingButton />
+          </div>
         </div>
-
-        <div className="flex justify-center h-18 w-18  p-2">
-          <SettingButton/>
+        <div className="h-20 w-50 flex justify-center items-center">
+          <Search />
         </div>
       </div>
-      
-
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/Search.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { FaMicrophone } from "react-icons/fa";
+
+const Search = () => {
+  const [voiceActive, setVoiceActive] = useState(false);
+  const [voiceText, setVoiceText] = useState("");
+
+  //   const toggleSearch = () => {
+  //     setVoiceActive(!voiceActive);
+  //   };
+
+  //   const handleSearch = () => {
+  //     if ('SpeechRecognition' in window) {
+  //       const recognition = new window.SpeechRecognition();
+  //       recognition.continuous = false;
+
+  //       recognition.onresult = (event) => {
+  //         const transcript = event.results[0][0].transcript;
+  //         setVoiceText(transcript);
+  //       };
+
+  //       recognition.onerror = (event) => {
+  //         console.log('Error', event.error);
+  //       };
+
+  //       recognition.onend = () => {
+  //         setVoiceActive(false);
+  //       };
+
+  //       recognition.start();
+  //     } else {
+  //       console.error('SpeechRecognition not supported by this browser');
+  //     }
+  //   };
+
+  return (
+    <div className="relative border-black border-1">
+      <input
+        type="text"
+        placeholder="Ask your question..."
+        value={voiceActive ? voiceText : ""}
+        onChange={(event) => setVoiceText(event.target.value)}
+        className="w-64 px-4 py-2 text-white rounded-md focus:outline-none"
+      />
+      <button
+        // onClick={toggleSearch}
+        className="absolute px-4 py-2 h-10 bg-gray-700 text-white rounded-md"
+      >
+        <FaMicrophone />
+      </button>
+      {voiceActive && (
+        <button
+          //   onClick={handleSearch}
+          className="absolute top-0 right-16 px-4 py-2 bg-gray-700 text-white rounded-md"
+        >
+          Speak
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
